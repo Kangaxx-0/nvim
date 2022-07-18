a0eca96c1511dda7f07629fb3ebee78be0507abb
Frank <v-frankz@microsoft.com>
2022-07-17 18:21:30 -0700

    format on save

--- lua/gaxx/lsp/null-ls.lua
+++ lua/gaxx/lsp/null-ls.lua
@@ -9,10 +9,31 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 -- local completion = null_ls.builtins.completion
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup {
   debug = false,
   sources = {
+    formatting.rustfmt.with(
+      {
+        extra_args = function(params)
+          local Path = require("plenary.path")
+          local cargo_toml = Path:new(params.root .. "/" .. "Cargo.toml")
+
+          if cargo_toml:exists() and cargo_toml:is_file() then
+            for _, line in ipairs(cargo_toml:readlines()) do
+              local edition = line:match([[^edition%s*=%s*%"(%d+)%"]])
+              if edition then
+                return { "--edition=" .. edition }
+              end
+            end
+
+          end
+          -- default edition when we don't find `Cargo.toml` or the `edition` in it.
+          return { "--edition=2021" }
+        end,
+      }
+    ),
     formatting.prettier,
     formatting.stylua,
     -- formatting.gofmt,
@@ -20,4 +41,22 @@ null_ls.setup {
     -- diagnostics.golangci_lint,
     code_actions.eslint,
   },
+  on_attach = function(client, bufnr)
+    if client.name == "rust_analyzer" or client.name == "tsserver" then
+      client.server_capabilities.document_formatting = false -- 0.7 and earlier
+      -- client.server_capabilities.documentFormattingProvider = false -- 0.8 and later
+    end
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+
+        buffer = bufnr,
+        callback = function()
+          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+          vim.lsp.buf.formatting_sync()
+        end,
+      })
+    end
+  end,
 }
