e82c7d61f4e6a496d226c1bf171ee8fc00af761f
Gaxx <damn.frank2z@gmail.com>
2023-07-24 14:45:25 -0700

    Fix clipboard lua in wsl2

--- lua/gaxx/clipboard.lua
+++ lua/gaxx/clipboard.lua
@@ -8,8 +8,13 @@ end
 
 if has("wsl") then
   local clip = "/mnt/c/Windows/System32/clip.exe"
-  local pwsh = "/mnt/c/Program Files/PowerShell/7/pwsh.exe"
-  local paste = pwsh .. ' -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))'
+  --[[ local pwsh = "\"/mnt/c/Program Files/PowerShell/7/pwsh.exe\"" ]]
+  --[[ local paste = pwsh .. ' -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))' ]]
+  -- code of pwsh_clip.sh, and make sure powershell 7 is installed
+  --   #!/bin/bash
+  --   "/mnt/c/Program Files/PowerShell/7/pwsh.exe" -c "[Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('`r', ''))"
+  --
+  local paste = "/home/gaxx/pwsh_clip.sh"
   vim.g.clipboard = {
     name = "WslClipboard",
     copy = {
